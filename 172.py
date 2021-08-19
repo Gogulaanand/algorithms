cffd398424cf064dc4795b1092c0b9970e087379
Gogulaanand R <gogulaanand02@gmail.com>
2021-08-19 09:54:02 +0530

    19-8-21

---
+++ 172.py
@@ -0,0 +1,9 @@
+https://leetcode.com/problems/factorial-trailing-zeroes/
+
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        r = 0
+        while n>0:
+            n = n//5
+            r += n
+        return r
