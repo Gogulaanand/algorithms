056f5b0cbc7e7ba426937cb6160b27e352fe968d
Gogulaanand R <gogulaanand02@gmail.com>
2021-09-13 08:46:33 +0530

    13-9-21

---
+++ 603.py
@@ -0,0 +1,23 @@
+https://leetcode.com/explore/featured/card/top-interview-questions-easy/93/linked-list/603/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        if head.next is None:
+            return None
+        slow = head
+        fast = head
+        for i in range(n):
+            fast = fast.next
+        if not fast:
+            return head.next
+        while fast.next:
+            slow = slow.next
+            fast = fast.next
+        
+        slow.next = slow.next.next
+        return head
